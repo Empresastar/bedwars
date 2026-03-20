b0fc4d368c9cba1120a161c13d215107ef803dc8
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 21:11:19 -0300

    Refactor Block class texture method and properties

--- css/js/block.js
+++ css/js/block.js
@@ -1,19 +1,18 @@
-// js/block.js
 import * as THREE from 'three';
 
 export class Block {
     constructor(x, y, z, type = 'grass') {
         this.position = new THREE.Vector3(x, y, z);
-        this.type = type; // 'grass', 'dirt', 'stone', etc.
-        this.mesh = null; // A malha 3D do bloco será criada em outro lugar
+        this.type = type;
+        this.mesh = null;
     }
 
-    // Você pode adicionar métodos aqui para pegar a textura do bloco, etc.
-    getTexturePath() {
+    getTextureColor() {
+        // Como você ainda não tem as imagens .png, vamos usar cores para não dar erro 404 de imagem
         switch (this.type) {
-            case 'grass': return 'assets/textures/grass_block_top.png';
-            case 'dirt': return 'assets/textures/dirt.png';
-            default: return 'assets/textures/missing_texture.png';
+            case 'grass': return 0x4CAF50; // Verde
+            case 'dirt': return 0x8B4513;  // Marrom
+            default: return 0xffffff;
         }
     }
 }
