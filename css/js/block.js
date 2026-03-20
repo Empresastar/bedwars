0a5acd1a93e7183ab4a9ddcabd6bfb4a4d29d61e
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 21:28:42 -0300

    Update block.js

--- css/js/block.js
+++ css/js/block.js
@@ -5,12 +5,10 @@ export class Block {
         this.position = new THREE.Vector3(x, y, z);
         this.type = type;
     }
-
     getColor() {
         switch (this.type) {
             case 'grass': return 0x48a044;
             case 'dirt': return 0x866043;
-            case 'bed': return 0xe22727; // Cor para o Bedwars
             default: return 0xffffff;
         }
     }
