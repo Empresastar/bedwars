d19554b0b59fdb31f71a4465e1b031223465aec6
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 21:19:10 -0300

    Rename getTextureColor to getColor and update colors

--- css/js/block.js
+++ css/js/block.js
@@ -4,14 +4,13 @@ export class Block {
     constructor(x, y, z, type = 'grass') {
         this.position = new THREE.Vector3(x, y, z);
         this.type = type;
-        this.mesh = null;
     }
 
-    getTextureColor() {
-        // Como você ainda não tem as imagens .png, vamos usar cores para não dar erro 404 de imagem
+    getColor() {
         switch (this.type) {
-            case 'grass': return 0x4CAF50; // Verde
-            case 'dirt': return 0x8B4513;  // Marrom
+            case 'grass': return 0x48a044;
+            case 'dirt': return 0x866043;
+            case 'bed': return 0xe22727; // Cor para o Bedwars
             default: return 0xffffff;
         }
     }
